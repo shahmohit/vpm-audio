3a0714f696696bfd85ccba6848beffd3739a913c
Mohit Shah <mohit.shah@asu.edu>
2015-04-08 15:23:52 -0700

    Fixed SSH

--- phase1/setup_ssh.sh
+++ phase1/setup_ssh.sh
@@ -3,6 +3,8 @@ echo "Press Enter when prompted for passphrase"
 ssh-keygen -f tmp/sensor
 ssh-keygen -f tmp/server
 echo "Enter root as password"
-ssh-copy-id -i tmp/sensor.pub -p 2222 root@localhost
+ssh-copy-id -i tmp/sensor.pub  "root@localhost -p 2222"
+cp tmp/sensor.pub $HOME/.ssh/
 echo "Enter platypus as password"
-ssh-copy-id -i tmp/server.pub -p 2223 mohit@localhost
+ssh-copy-id -i tmp/server.pub  "mohit@localhost -p 2223"
+cp tmp/server.pub $HOME/.ssh/
