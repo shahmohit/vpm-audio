f0188df65ded17a52b8d1148f4d2bad12ea7e88b
Mohit Shah <mohit.shah@asu.edu>
2015-04-24 16:17:19 -0700

    Logging VMs and watcher

--- phase1/launch_all.sh
+++ phase1/launch_all.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 echo "Launcing Sensor..."
-nohup ./launch_sensor.sh > /dev/null < /dev/null &
+nohup ./launch_sensor.sh > tmp/sensor.log 2> tmp/sensor.err < /dev/null &
 echo "Done."
 echo "Launching Phone..."
-nohup ./launch_phone.sh > /dev/null < /dev/null &
+nohup ./launch_phone.sh > tmp/phone.log 2> phone.err < /dev/null &
 echo "Done."
 echo "Launching Server..."
-nohup ./launch_server.sh > /dev/null < /dev/null &
+nohup ./launch_server.sh > tmp/server.log 2> server.err < /dev/null &
 echo "Done."
 echo "Launching Watcher Script..."
-nohup ./start_watcher.sh > /dev/null < /dev/null &
+nohup ./start_watcher.sh > tmp/watcher.log 2> watcher.err < /dev/null &
 echo "Done."
 
